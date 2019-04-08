e46b24035bd422d71cd54deec01743a3adde4291
David Wosk <davidwosk1@gmail.com>
2019-04-08 15:03:30 -0700

    feat: Verify token id using google supported library

--- utils/auth.js
+++ utils/auth.js
@@ -1,15 +1,25 @@
+const { OAuth2Client } = require('google-auth-library');
+const client = new OAuth2Client('206945578523-0h8t8i7k5d09j0vg31ncspa4pbrddff6.apps.googleusercontent.com');
+let core = require('../api/internal/core');
+
 module.exports = async function (request, response, next) {
   try {
     const { authorization } = request.headers;
     if (!authorization) throw new Error('You must send an Authorization header');
-    
+
     const [authType, token] = authorization.trim().split(' ');
     if (authType !== 'Bearer') throw new Error('Expected a Bearer token');
-    
-    // TODO: Verify id token
+
     // https://developers.google.com/identity/sign-in/web/backend-auth
-    console.log(token);
-    next();
+    const ticket = await client.verifyIdToken({
+      idToken: token,
+      audience: '206945578523-0h8t8i7k5d09j0vg31ncspa4pbrddff6.apps.googleusercontent.com'
+    });
+
+    const payload = ticket.getPayload();
+    const googleId = payload['sub'];
+    console.log(`API request from ${googleId}`)
+    next(googleId);
   } catch (error) {
     next(error.message);
   }
