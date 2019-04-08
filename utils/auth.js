45f1af76a237a85fb7184fd24da8a1ffe44641ee
David Wosk <davidwosk1@gmail.com>
2019-04-08 13:00:01 -0700

    feat: Initial framework for securing endpoints

--- utils/auth.js
+++ utils/auth.js
@@ -1,5 +1,16 @@
-module.exports = function (request, response, next) {
-  // TODO: Check if user is logged in
-  console.log('Full perm mode...');
-  next();
+module.exports = async function (request, response, next) {
+  try {
+    const { authorization } = request.headers;
+    if (!authorization) throw new Error('You must send an Authorization header');
+    
+    const [authType, token] = authorization.trim().split(' ');
+    if (authType !== 'Bearer') throw new Error('Expected a Bearer token');
+    
+    // TODO: Verify id token
+    // https://developers.google.com/identity/sign-in/web/backend-auth
+    console.log(token);
+    next();
+  } catch (error) {
+    next(error.message);
+  }
 };
