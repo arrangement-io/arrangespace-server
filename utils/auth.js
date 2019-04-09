385be52f6524353024b1312d9b2368277796c282
David Wosk <davidwosk1@gmail.com>
2019-04-09 11:14:42 -0700

    fix: Return json error when missing authentication

--- utils/auth.js
+++ utils/auth.js
@@ -8,10 +8,30 @@ module.exports = async function (request, response, next) {
   try {
     if (request.path !== '/health') {
       const { authorization } = request.headers;
-      if (!authorization) throw new Error('You must send an Authorization header');
+      if (!authorization) {
+        let error = {
+          error: {
+            status: 401,
+            reason: 'Unauthorized',
+            message: 'You must send an Authorization header'
+          }
+        };
+        core.sendFailureResponse(error, response);
+        return;
+      }
 
       const [authType, token] = authorization.trim().split(' ');
-      if (authType !== 'Bearer') throw new Error('Expected a Bearer token');
+      if (authType !== 'Bearer') {
+        let error = {
+          error: {
+            status: 401,
+            reason: 'Unauthorized',
+            message: 'Expected a Bearer token'
+          }
+        };
+        core.sendFailureResponse(error, response);
+        return;
+      }
 
       // https://developers.google.com/identity/sign-in/web/backend-auth
       const ticket = await client.verifyIdToken({
