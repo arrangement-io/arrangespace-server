d4c48ba357cd39cb983b6ab172508cf16c8d2a4c
David Wosk <davidwosk1@gmail.com>
2019-04-09 11:11:12 -0700

    fix: Whitelist /health endpoint

--- utils/auth.js
+++ utils/auth.js
@@ -6,35 +6,38 @@ const WHITELIST_DOMAINS = ['gpmail.org'];
 
 module.exports = async function (request, response, next) {
   try {
-    const { authorization } = request.headers;
-    if (!authorization) throw new Error('You must send an Authorization header');
+    if (request.path !== '/health') {
+      const { authorization } = request.headers;
+      if (!authorization) throw new Error('You must send an Authorization header');
 
-    const [authType, token] = authorization.trim().split(' ');
-    if (authType !== 'Bearer') throw new Error('Expected a Bearer token');
+      const [authType, token] = authorization.trim().split(' ');
+      if (authType !== 'Bearer') throw new Error('Expected a Bearer token');
 
-    // https://developers.google.com/identity/sign-in/web/backend-auth
-    const ticket = await client.verifyIdToken({
-      idToken: token,
-      audience: GOOGLE_CLIENT_ID
-    });
+      // https://developers.google.com/identity/sign-in/web/backend-auth
+      const ticket = await client.verifyIdToken({
+        idToken: token,
+        audience: GOOGLE_CLIENT_ID
+      });
 
-    const payload = ticket.getPayload();
-    const googleId = payload['sub'];
-    const domain = payload['hd'];
+      const payload = ticket.getPayload();
+      const googleId = payload['sub'];
+      const domain = payload['hd'];
 
-    if (WHITELIST_DOMAINS.indexOf(domain) === -1) {
-      let error = {
-        error: {
-          status: 401,
-          reason: 'Unauthorized',
-          message: 'Invalid domain'
-        }
-      };
-      core.sendFailureResponse(error, response);
-      return;
+      if (WHITELIST_DOMAINS.indexOf(domain) === -1) {
+        let error = {
+          error: {
+            status: 401,
+            reason: 'Unauthorized',
+            message: 'Invalid domain'
+          }
+        };
+        core.sendFailureResponse(error, response);
+        return;
+      }
+
+      request.googleId = googleId;
     }
 
-    request.googleId = googleId;
     next();
   } catch (error) {
     next(error.message);
