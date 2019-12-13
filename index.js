4bf497f0cfed34adc8d45836c97c19f79dbf3732
Gideon Chia <gchia@gchia-mn2.linkedin.biz>
2019-12-13 10:50:36 -0800

    Set a persistent memory store

--- index.js
+++ index.js
@@ -11,15 +11,24 @@ const core = require('./api/internal/core');
 let path = require('path');
 let db = require('./db/database_mongo');
 const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
+const { MONGODB_URL, MONGODB_NAME } = process.env;
 const passport = require('passport');
 require('./passport_init');
 
 const ALLOWED_ORIGINS = ['http://localhost:3000', 'http://localhost:3001', 'https://arrange.space'];
 
+const sessionStore = new MongoDBStore({
+  uri: `mongodb://${MONGODB_URL}`,
+  databaseName: MONGODB_NAME,
+  collection: 'expressSessions'
+});
+
 app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: true,
-  saveUninitialized: true
+  saveUninitialized: true,
+  store: sessionStore
 }));
 app.use(passport.initialize());
 app.use(passport.session());
