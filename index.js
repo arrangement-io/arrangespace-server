935670acc8315911745a8d5549e65b50f24cfaa8
Gideon <deonjo@gmail.com>
2019-12-17 16:18:54 -0800

    Hotfix login (#19)
    * added constants
    
    * Set a persistent memory store
    
    * save the redirect to the session

--- index.js
+++ index.js
@@ -11,27 +11,49 @@ const core = require('./api/internal/core');
 let path = require('path');
 let db = require('./db/database_mongo');
 const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
+const { MONGODB_URL, MONGODB_NAME } = process.env;
 const passport = require('passport');
 require('./passport_init');
 
 const ALLOWED_ORIGINS = ['http://localhost:3000', 'http://localhost:3001', 'https://arrange.space'];
 
+const sessionStore = new MongoDBStore({
+  uri: `mongodb://${MONGODB_URL}`,
+  databaseName: MONGODB_NAME,
+  collection: 'expressSessions'
+});
+
 app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: true,
-  saveUninitialized: true
+  saveUninitialized: true,
+  store: sessionStore
 }));
 app.use(passport.initialize());
 app.use(passport.session());
 
 app.get('/auth/google',
+  function (req, res, next) {
+    if (req.session.bounceTo) { // already have a bounce destination
+      return next();
+    } else {
+      if (req.query['bounce']) {
+        req.session.bounceTo = req.query['bounce'];
+      } else { // no explicit destination, use referer or homepage
+        req.session.bounceTo = req.header('Referer') || '/';
+      }
+      return next();
+    };
+  },
   passport.authenticate('google', { scope: ['profile', 'email'] }));
 
 app.get('/auth/google/callback',
   passport.authenticate('google', { failureRedirect: 'back' }),
   function (req, res) {
     core.log(`User authenticated ${req.user.googleId}`);
-    res.redirect('back');
+    res.redirect(req.session.bounceTo || '/');
+    delete req.session.bounceTo;
   });
 
 app.use(cors({
