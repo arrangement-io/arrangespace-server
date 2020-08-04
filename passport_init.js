9598c894a891d744936e267b462109461463ebd5
Gideon <deonjo@gmail.com>
2020-08-03 23:14:32 -0700

    Whitelist/jenny (#25)
    * Whitelisting Jenny Chen
    
    * New push
    
    Co-authored-by: Gideon Chia <gchia@gchia-mn2.linkedin.biz>

--- passport_init.js
+++ passport_init.js
@@ -1,5 +1,6 @@
 const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 const passport = require('passport');
+const core = require('./api/internal/core');
 let db = require('./db/database_mongo');
 
 const WHITELIST_DOMAINS = ['gpmail.org'];
@@ -21,6 +22,16 @@ const WHITELIST_EMAILS = [
   'jyen83@gmail.com',
   'jenny.chen@gpmail.org'];
 
+const getDomain = (email) => {
+  if (email !== undefined) {
+    const parts = email.split(`@`);
+    if (parts.length === 2) {
+      return parts[1];
+    }
+  }
+  core.log(`email does not have domain: ${email}`);
+};
+
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_SECRET,
@@ -28,8 +39,8 @@ passport.use(new GoogleStrategy({
 },
 function (token, tokenSecret, profile, cb) {
   if (profile) {
-    const email = profile.emails[0].value;
-    const domain = profile._json.hd;
+    const email = profile._json.email || profile.emails[0].value;
+    const domain = profile._json.hd || getDomain(email);
     const user = { googleId: profile.id,
       name: profile.displayName,
       givenName: profile.name.givenName,
@@ -37,13 +48,16 @@ function (token, tokenSecret, profile, cb) {
       imageUrl: profile._json.picture,
       email: email
     };
+    core.log(`domain: ${domain}`);
+    core.log(`whitelist domain: ${WHITELIST_DOMAINS.indexOf(domain)}`);
+    core.log(profile._json);
     if (WHITELIST_DOMAINS.indexOf(domain) === -1 && WHITELIST_EMAILS.indexOf(email) === -1) {
-      return cb(new Error('Invalid Domain'));
+      return cb(new Error(`Invalid Domain: ${domain} and Invalid Email: ${email}`));
     }
     db.createUserByGoogleId({ user_data: user });
     return cb(null, user);
   }
-  return cb(new Error('Could not create or find user'));
+  return cb(new Error(`Could not create or find user with profile: ${profile}`));
 }));
 
 passport.serializeUser(function (user, done) {
